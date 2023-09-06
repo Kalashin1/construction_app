752c22610e5e480a9640305928b0ffcf14657e61
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-09-06 22:39:00 +0100

    worked on project details page

---
+++ src/pages/dashboard/projects/details/components/construction-schedule.tsx
@@ -0,0 +1,95 @@
+const ConstructionSchedule = () => {
+  return (
+    <div className="w-full">
+      <h3 className="my-6 text-lg font-bold">Construction schedule</h3>
+      <ol className="timeline bg-white rounded-md shadow p-6 w-full">
+        <li className="timeline-item">
+          <div
+            className="timeline-item-point rounded-full bg-slate-300 dark:bg-navy-400"
+          ></div>
+          <div className="timeline-item-content flex-1 pl-4 sm:pl-8">
+            <div className="flex flex-col justify-between pb-2 sm:flex-row sm:pb-0">
+              <p
+                className="pb-2 font-medium leading-none text-slate-600 dark:text-navy-100 sm:pb-0"
+              >
+                User Photo Changed
+              </p>
+              <span className="text-xs text-slate-400 dark:text-navy-300"
+              >12 minute ago</span>
+            </div>
+            <p className="py-1">John Doe changed his avatar photo</p>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <div
+            className="timeline-item-point rounded-full bg-primary dark:bg-accent"
+          ></div>
+          <div className="timeline-item-content flex-1 pl-4 sm:pl-8">
+            <div className="flex flex-col justify-between pb-2 sm:flex-row sm:pb-0">
+              <p
+                className="pb-2 font-medium leading-none text-slate-600 dark:text-navy-100 sm:pb-0"
+              >
+                Video Added
+              </p>
+              <span className="text-xs text-slate-400 dark:text-navy-300"
+              >1 hour ago</span>
+            </div>
+            <p className="py-1">Mores Clarke added new video</p>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <div className="timeline-item-point rounded-full bg-success">
+            <span
+              className="inline-flex h-full w-full animate-ping rounded-full bg-success opacity-80"
+            ></span>
+          </div>
+          <div className="timeline-item-content flex-1 pl-4 sm:pl-8">
+            <div className="flex flex-col justify-between pb-2 sm:flex-row sm:pb-0">
+              <p
+                className="pb-2 font-medium leading-none text-slate-600 dark:text-navy-100 sm:pb-0"
+              >
+                Design Completed
+              </p>
+              <span className="text-xs text-slate-400 dark:text-navy-300"
+              >3 hours ago</span>
+            </div>
+            <p className="py-1">
+              Robert Nolan completed the design of the CRM application
+            </p>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <div className="timeline-item-point rounded-full bg-warning"></div>
+          <div className="timeline-item-content flex-1 pl-4 sm:pl-8">
+            <div className="flex flex-col justify-between pb-2 sm:flex-row sm:pb-0">
+              <p
+                className="pb-2 font-medium leading-none text-slate-600 dark:text-navy-100 sm:pb-0"
+              >
+                ER Diagram
+              </p>
+              <span className="text-xs text-slate-400 dark:text-navy-300"
+              >a day ago</span>
+            </div>
+            <p className="py-1">Team completed the ER diagram app</p>
+          </div>
+        </li>
+        <li className="timeline-item">
+          <div className="timeline-item-point rounded-full bg-error"></div>
+          <div className="timeline-item-content flex-1 pl-4 sm:pl-8">
+            <div className="flex flex-col justify-between pb-2 sm:flex-row sm:pb-0">
+              <p
+                className="pb-2 font-medium leading-none text-slate-600 dark:text-navy-100 sm:pb-0"
+              >
+                Weekly Report
+              </p>
+              <span className="text-xs text-slate-400 dark:text-navy-300">a day ago</span>
+            </div>
+            <p className="py-1">The weekly report was uploaded</p>
+          </div>
+        </li>
+      </ol>
+    </div>
+  )
+}
+
+export default ConstructionSchedule;
---
+++ src/pages/dashboard/projects/details/components/scope-of-service.tsx
@@ -0,0 +1,22 @@
+const ScopeOfService = () => {
+  return (
+    <div>
+      <h3 className="my-6 text-lg font-bold">Scope of services</h3>
+
+      <div className="bg-white rounded-md shadow w-full">
+        <div className="p-6 flex flex-col md:flex-row justify-between">
+          <h3>Actions & Filters</h3>
+
+          <button className="my-4 md:my-0 bg-transparent border-gray-800 border py-1 px-2 md:px-4 rounded-md">Multiple Selection</button>
+        </div>
+        <div className="h-px flex-1 bg-slate-200 dark:bg-navy-500"></div>
+        <div className="flex p-6 flex-col md:flex-row justify-between">
+          <button className="my-4 md:my-0 bg-transparent border-gray-800 border py-1 px-2 md:px-4 rounded-md">Complete all Positions</button>
+          <button className="my-4 md:my-0 bg-transparent border-gray-800 border py-1 px-2 md:px-4 rounded-md">Show only your own positions</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScopeOfService;
