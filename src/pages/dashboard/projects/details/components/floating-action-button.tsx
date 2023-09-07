11b81766178b05d5e677353e058184ddee614b9a
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-09-07 19:23:51 +0100

    added floating action button

--- src/pages/dashboard/projects/details/components/floating-action-button.tsx
+++ src/pages/dashboard/projects/details/components/floating-action-button.tsx
@@ -1,4 +1,4 @@
-import {Chat, ChatNew} from "../../../svg";
+import {Chat} from "../../../svg";
 
 const FloatingActionButton = () => (
   <div className="rounded-full bg-gray-300 flex justify-center items-center fixed bottom-2 right-10 w-14 h-14 self-end shadow-md">
