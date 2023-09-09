a6998f3b2ffe6d888e6d4e5367b5b0b34fabbdbd
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-09-09 19:14:11 +0100

    work on asus PC

--- src/pages/dashboard/projects/details/components/main-order-item.tsx
+++ src/pages/dashboard/projects/details/components/main-order-item.tsx
@@ -1,8 +1,13 @@
+import { ChevronRightIcon } from "../svgs";
+
 const MainOrderItem = () => {
   return (
     <div>
       <div className="bg-white shadow-md rounded p-6">
-
+        <div>
+          <ChevronRightIcon color="#000" width={40} />
+          <h3>Main order items</h3>
+        </div>
       </div>
     </div>
   );
--- src/pages/dashboard/projects/details/svgs/index.ts
+++ src/pages/dashboard/projects/details/svgs/index.ts
@@ -3,7 +3,7 @@ import ContactPersonIcon from "./contact-person";
 import AdminIcon from "./admin";
 import HouseIcon from "./house";
 import PDFIcon from "./pdf";
-
+import ChevronRightIcon from "./chevron-right";
 
 export {
   CaretakerIcon,
@@ -11,4 +11,5 @@ export {
   AdminIcon,
   HouseIcon,
   PDFIcon,
+  ChevronRightIcon,
 }
