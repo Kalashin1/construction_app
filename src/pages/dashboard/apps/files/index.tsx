ea121d8cfbeb15fc9a1d36bbc17fb4ae4b033fde
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-09-12 13:18:59 +0100

    finished file manager sidebar

--- src/pages/dashboard/apps/files/index.tsx
+++ src/pages/dashboard/apps/files/index.tsx
@@ -1,9 +1,33 @@
 import Layout from "../../layout";
-
+import SidebarPanel from "./components/sidepanel";
+import { useContext } from "react";
+import { SidebarContext } from "../../../../App";
 
 const FileManager = () => {
+  const {
+    deviceWidth,
+    showProjectMenu,
+    updateShowProjectMenu,
+    showSidebar,
+    updateShowSidebar
+  } = useContext(SidebarContext);
   return (
-    <Layout>
+    <Layout
+      sidePanel={
+        (
+          <SidebarPanel
+
+            closeSidebar={
+              deviceWidth && deviceWidth > 560 ?
+                () => updateShowProjectMenu && updateShowProjectMenu(!showProjectMenu) :
+                () => updateShowSidebar && updateShowSidebar(!showSidebar)
+            }
+            links={[0, 1, 2, 3]}
+            headerText="Files"
+          />
+        )
+      }
+    >
       <main></main>
     </Layout>
   )
